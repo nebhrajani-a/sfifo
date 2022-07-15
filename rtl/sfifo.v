fdaf1cb80983d4eff93ca0536f693de3259c3463
nebu <aditya.v.nebhrajani@gmail.com>
2022-07-16 01:50:28 +0530

    RTL --- fix empty flag behavior.
    The test sfifo_rw_tst still does not pass: it expects more data than
    is given, and exp_data does not account for burst reads (word_cnt == 0)
    failing.

--- rtl/sfifo.v
+++ rtl/sfifo.v
@@ -44,7 +44,7 @@ module sfifo
   reg [7:0]      fifo [63:0];
   reg [5:0]      write_ptr;
   reg [5:0]      read_ptr;
-  reg [6:0]      fifo_size;
+  reg [6:0]      word_cnt;
 
   // Flopins
   reg            w_en_reg;
@@ -80,83 +80,67 @@ module sfifo
         begin
           write_ptr <= 'h0;
           read_ptr  <= 'h0;
-          fifo_size <= 'h0;
+          word_cnt <= 'h0;
           dout      <= 'h0;
           overflow  <= 1'b0;
           underflow <= 1'b0;
+          empty     <= 1'b1;
+          full      <= 1'b0;
         end
       else
         begin
+
           // Write logic
-          if (w_en_reg == 1'b1)
+          if ((w_en_reg == 1'b1) && (r_en_reg == 1'b0))
             begin
-              if (fifo_size !== 7'd65)
+              if (word_cnt !== 7'd64)
                 begin
                   fifo[write_ptr] <= din_reg;
                   write_ptr <= write_ptr + 1'b1;
-                  underflow <= 1'b0;
-                  if (r_en_reg == 1'b0)
-                    begin
-                      fifo_size <= fifo_size + 1'b1;
-                    end
+                  empty <= 1'b0;
+                  word_cnt <= word_cnt + 1'b1;
                 end
               else
                 begin
                   overflow <= 1'b1;
                 end
-            end // if (w_en_reg == 1'b1)
+            end // if ((w_en_reg == 1'b1) && (r_en_reg == 1'b0))
+
           // Read logic
-          if (r_en_reg == 1'b1)
+          if ((r_en_reg == 1'b1) && (w_en_reg == 1'b0) && (empty == 1'b0))
             begin
-              if (fifo_size !== 7'd0)
+              if (word_cnt !== 7'd0)
                 begin
                   dout <= fifo[read_ptr];
                   read_ptr <= read_ptr + 1'b1;
-                  overflow <= 1'b0;
-                  if (w_en_reg == 1'b0)
-                    begin
-                      fifo_size <= fifo_size - 1'b1;
-                    end
+                  empty <= (word_cnt == 7'd1) ? 1'b1 : 1'b0;
+                  word_cnt <= word_cnt - 1'b1;
                 end
               else
                 begin
                   underflow <= 1'b1;
-                end
-            end // if (r_en_reg == 1'b1)
+                end // else: !if(word_cnt !== 7'd0)
+            end // if ((r_en_reg == 1'b1) && (w_en_reg == 1'b0))
+
+          // Read && write logic
+          if ((r_en_reg == 1'b1) && (w_en_reg == 1'b1))
+              begin
+                if (empty == 1'b1)
+                  begin
+                    word_cnt <= 7'd1;
+                    fifo[write_ptr] <= din_reg;
+                    write_ptr <= write_ptr + 1'b1;
+                    empty <= 1'b0;
+                  end
+                else
+                  begin
+                    fifo[write_ptr] <= din_reg;
+                    write_ptr <= write_ptr + 1'b1;
+                    dout <= fifo[read_ptr];
+                    read_ptr <= read_ptr + 1'b1;
+                  end
+              end
         end // else: !if(rst == 1'b0)
     end // always @ (posedge clk or negedge rst)
-
-  //----------------------------------------------------------------------
-  // Flag logic
-  //----------------------------------------------------------------------
-  always @(posedge clk or negedge rst)
-    begin
-      if (rst == 1'b0)
-        begin
-          empty     <= 1'b1; // how does this synthesize?
-          full      <= 1'b0;
-        end
-      else
-        begin
-          if (fifo_size <= 7'd2 &&
-              w_en_reg == 1'b0)
-            begin
-              empty <= 1'b1;
-            end
-          else
-            begin
-              empty <= 1'b0;
-            end
-          if (fifo_size >= 7'd62)
-            begin
-              full <= 1'b1;
-            end
-          else
-            begin
-              full <= 1'b0;
-            end
-        end
-    end
-
 
 endmodule
